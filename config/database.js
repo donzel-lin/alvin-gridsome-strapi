22a39850857aae4b05a7b2d8f01b40a96242bd20
Your Name <you@example.com>
2022-08-11 14:33:05 +0800

    mysql

--- config/database.js
+++ config/database.js
@@ -1,15 +1,33 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', '120.46.147.119'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'blog'),
+        username: env('DATABASE_USERNAME', 'blogAlvin'),
+        password: env('DATABASE_PASSWORD', 'blogAlvin@1'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
