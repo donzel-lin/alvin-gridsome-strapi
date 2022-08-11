a015d17f776830b7acb04aa69ae3ddaa497ce393
Your Name <you@example.com>
2022-08-11 14:19:51 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a81307236282721f8e708331dbaf33c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5ae2c8d9-b654-4c70-9c6a-991b10c1c91e'
+};
